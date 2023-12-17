6281d03031ace6dac88a29fee1b6cabd94d25a2a
m-lind <mari.j.lindstedt@gmail.com>
2023-12-17 16:03:49 +0200

    Added an endpoint for the exercise calculator

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express = require("express");
 import { calculateBmi } from "./bmiCalculator";
+import { calculateExercises, hours } from "./exerciseCalculator";
 
 const app = express();
 
@@ -19,6 +20,31 @@ app.get("/bmi", (req, res) => {
   } else {
     res.send({ error: "malformatted parameters" });
   }
+});
+
+app.use(express.json());
+
+app.post("/exercises", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { daily_exercises, target } = req.body;
+
+  if (!daily_exercises || !target) {
+    return res.status(400).json({ error: "parameters missing" });
+  }
+
+  const daily_exercises_array: number[] = (daily_exercises as hours)
+    .map(hour => Number(hour))
+    .filter(parsedHour => !isNaN(parsedHour));
+
+  if (
+    daily_exercises_array.length !== (daily_exercises as hours).length ||
+    isNaN(Number(target))
+  ) {
+    return res.status(400).json({ error: "malformatted parameters" });
+  }
+
+  const result = calculateExercises(Number(target), daily_exercises_array);
+  return res.json({ result });
 });
 
 app.listen(PORT, () => {
